aa67b4492ce373fb11c0c5725ae67b5d4c95330d
Raushanray <raushankuray1998@gmail.com>
2024-06-09 17:23:13 +0530

    Start program with if case

---
+++ if_examples.py
@@ -0,0 +1,24 @@
+# x = 7
+#
+# if x > 3 and x < 6:
+#     # print(f"{x} is greater than 3")
+#     print(f"{x} is between 3 and 6")
+#     print("This is part of if")
+# else:
+#     print(f"{x} is not between 3 and 6")
+#     print("This is part of else")
+
+
+
+# else if
+x = 3
+
+if x == 1:
+    print(f"{x} is 1")
+    print("This is part of if")
+elif x == 2:
+    print(f"{x} is 1")
+    print("This is part of elif")
+else:
+    print(f"{x} is not 1")
+    print("This is part of else")
