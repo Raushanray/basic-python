6454681adc114d81819462a7a793b6d09b0d39ab
Raushanray <raushankuray1998@gmail.com>
2024-06-09 17:23:54 +0530

    Create a method in python

---
+++ first_method.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+def print_hello_world_twice():
+    print("Hello World 1")
+    print("Hello World 2")
+
+print_hello_world_twice()
---
+++ first_method_hello.py
@@ -0,0 +1,5 @@
+def print_hello_world_multiple_times(times):
+    for i in range(1, times+1):
+        print("Hello World")
+
+print_hello_world_multiple_times(4)
