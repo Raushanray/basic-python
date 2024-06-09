32a253d588f8d05ce81057e77578682992316ee2
Raushanray <raushankuray1998@gmail.com>
2024-06-09 17:20:22 +0530

    perform the arithmetic operations

---
+++ menu_with_if.py
@@ -0,0 +1,29 @@
+number1  = int(input("Enter Number1: "))
+number2  = int(input("Enter Number2: "))
+
+print("Choices Available are")
+print("1 - Add")
+print("2 - Subtract")
+print("3 - Divide")
+print("4 - Multiply")
+
+
+choice  = int(input("Enter Choice: "))
+
+
+print("Your Choices are")
+print(f"Number1 {number1}")
+print(f"Number1 {number2}")
+print(f"Selected Choice {choice}")
+
+
+if choice == 1:
+    print(f"Result = {number1 + number2}")
+elif choice == 2:
+    print(f"Result = {number1 - number2}")
+elif choice == 3:
+    print(f"Result = {number1 / number2}")
+elif choice == 4:
+    print(f"Result = {number1 * number2}")
+else:
+    print("Invalid Operation")
