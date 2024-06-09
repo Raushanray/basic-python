d40e7e2ef2dd1eb6658aefbbe32879759be14cdd
Raushanray <raushankuray1998@gmail.com>
2024-06-09 17:27:00 +0530

    Take an user input in python

---
+++ user_input.py
@@ -0,0 +1,12 @@
+x_string = input("Enter a Number: ")
+x = int(x_string)
+
+if x == 1:
+    print(f"{x} is 1")
+    print("This is part of if")
+elif x == 2:
+    print(f"{x} is 1")
+    print("This is part of elif")
+else:
+    print(f"{x} is not 1 or 2")
+    print("This is part of else")
