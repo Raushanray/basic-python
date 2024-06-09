e7618e8be2c1dc73c555d4f920cbad0c4ff8a60b
Raushanray <raushankuray1998@gmail.com>
2024-06-09 17:26:19 +0530

    Basic multiplications

---
+++ MultiplicationTable.py
@@ -0,0 +1,13 @@
+def print_multiplication_table(table=5, start= 1, end= 10):
+    for i in range(start, end+1):
+        print(f"{table} * {i} = {table*i}")
+
+# def print_multiplication_table(table):
+#     print_multiplication_table(table, 1, 10)
+
+
+print_multiplication_table()
+
+# print_multiplication_table(6)
+#
+# print_multiplication_table(7, 5, 10)
