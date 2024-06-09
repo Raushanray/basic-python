9829ee42510c2a899548a45dd936c37c5f51db4e
Raushanray <raushankuray1998@gmail.com>
2024-06-09 17:25:45 +0530

    Start working with while loop.

---
+++ do_while_solve_using_while.py
@@ -0,0 +1,11 @@
+# number = 0 # condition returns true
+# while number >= 0:
+#     number = int(input('Enter a number:'))
+#     print(f'cube is {number ** 3}')
+
+
+# 2nd way
+number = int(input('Enter a number:'))
+while number >= 0:
+    print(f'cube is {number ** 3}')
+    number = int(input('Enter a number:'))
